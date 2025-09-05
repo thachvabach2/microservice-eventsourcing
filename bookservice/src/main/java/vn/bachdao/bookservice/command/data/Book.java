dca7f5312e93efd368cc66c260a66b378c7d9c99
Bach Dao <ngocbach12a1@gmail.com>
2025-09-06 01:16:16 +0700

    [BS] feat: create book entity

---
+++ bookservice/src/main/java/vn/bachdao/bookservice/command/data/Book.java
@@ -0,0 +1,23 @@
+package vn.bachdao.bookservice.command.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String author;
+    private Boolean isReady;
+}
