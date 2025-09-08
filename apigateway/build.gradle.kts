35fa1747e8b9753cde2090f2bd0fdf33d785cbce
Bach Dao <ngocbach12a1@gmail.com>
2025-09-09 00:53:14 +0700

    [AG] conf: integrate rate limiter into spring cloud gateway and use apache jmeter to testing performace

--- apigateway/build.gradle.kts
+++ apigateway/build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2025.0.0"
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- apigateway/src/main/java/vn/bachdao/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/vn/bachdao/apigateway/ApigatewayApplication.java
@@ -3,6 +3,9 @@ package vn.bachdao.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,9 @@ public class ApigatewayApplication {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver keyResolver() {
+		// lấy địa chỉ ip làm key
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
 }
