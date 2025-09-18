1ff3321f56ecfe003c131302b297716515d08a18
Bach Dao <ngocbach12a1@gmail.com>
2025-09-18 19:41:29 +0700

    [BS+BrS+ES] build: add Axon Framework BOM to version catalog

--- bookservice/build.gradle.kts
+++ bookservice/build.gradle.kts
@@ -2,11 +2,12 @@ extra["springCloudVersion"] = libs.versions.spring.cloud.get()
 
 dependencies {
 	implementation(project(":commonservice"))
+	api(platform(libs.axon.bom))
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("org.axonframework:axon-spring-boot-starter:4.12.1")
+	implementation("org.axonframework:axon-spring-boot-starter")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
