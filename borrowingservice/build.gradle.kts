ecac769193d5b7c37385327915bf077a03ee9993
Bach Dao <ngocbach12a1@gmail.com>
2025-09-18 17:01:05 +0700

    [BrS] feat: init borrowing service

---
+++ borrowingservice/build.gradle.kts
@@ -0,0 +1,20 @@
+extra["springCloudVersion"] = libs.versions.spring.cloud.get()
+
+dependencies {
+    implementation(project(":commonservice"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
---
+++ borrowingservice/src/main/java/vn/bachdao/borrowingservice/BorrowingserviceApplication.java
@@ -0,0 +1,13 @@
+package vn.bachdao.borrowingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BorrowingserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BorrowingserviceApplication.class, args);
+    }
+}
