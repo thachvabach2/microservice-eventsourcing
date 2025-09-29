27fef78ccb2106baaa85a28045a336671322ac04
Bach Dao <ngocbach12a1@gmail.com>
2025-09-29 13:54:49 +0700

    [CICD] feat: update workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,8 @@ git checkout $BRANCH
 git pull
 
 docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml pull configserver
 docker compose -f docker-compose.yml pull discoverserver
+docker compose -f docker-compose.yml up -d configserver
 docker compose -f docker-compose.yml up -d discoverserver
 docker system prune -af
